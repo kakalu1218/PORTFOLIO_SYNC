88212de12b126d33f70e85a08c6854a295c13b2f
DESKTOP-JJDLHHS\PC <kakalu1218@gmail.com>
2023-09-06 04:20:37 +0900

    - Protobuf 셋팅

---
+++ Server/protoc-23.4-win64/readme.txt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cb87be42344000337bef2e65de178c7e2f9cd7a1b7cd0e4284377a5d375db82f
+size 721
---
+++ Unity_Client/Assets/Scripts/Managers/Managers.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    static Managers s_instance; // 유일성이 보장된다
+    static Managers Instance { get { Init(); return s_instance; } } // 유일한 매니저를 갖고온다
+
+    NetworkManager _network = new NetworkManager();
+    public static NetworkManager Network { get { return Instance._network; } }
+
+	void Start()
+    {
+        Init();
+	}
+
+    void Update()
+    {
+        _network.Update();
+    }
+
+    static void Init()
+    {
+        if (s_instance == null)
+        {
+			GameObject go = GameObject.Find("@Managers");
+            if (go == null)
+            {
+                go = new GameObject { name = "@Managers" };
+                go.AddComponent<Managers>();
+            }
+
+            DontDestroyOnLoad(go);
+            s_instance = go.GetComponent<Managers>();
+
+            s_instance._network.Init();
+        }		
+	}
+
+    public static void Clear()
+    {
+    }
+}
